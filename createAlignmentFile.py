2e5974b998efcb094ba183a75c8e62d982fdd00f
Gunaa AV <avgunaa@gmail.com>
2014-11-30 02:30:24 -0800

    small update

--- createAlignmentFile.py
+++ createAlignmentFile.py
@@ -14,24 +14,30 @@ def template1(kb_files, text_files):
     # for every pair of (kb_file, text_file) we compute the various feature counts
     # if they have atleast one intersection
     for kb_file in kb_files:
+
         fp1 = open(KB_TRIPLES_DIR + kb_file, 'r')
         kb_lines = set(fp1.readlines())
+
         for text_file in text_files:
+
             intersect_count = 0
             fp2 = open(TEXT_TRIPLES_DIR + text_file, 'r')
             text_lines = fp2.readlines()
             for line in text_lines:
                 if line in kb_lines:
                     intersect_count += 1
+
             features = {}
             features["INTERSECTION_SIZE"] = intersect_count
             features["KB_SIZE"] = len(kb_lines)
             features["TEXT_SIZE"] = len(text_lines)
+
             alignment = {}
             alignment['formula'] = kb_file
             alignment['source'] = 'Template1'
             alignment['features'] = features
             alignment['lexeme'] = text_file
+            
             if intersect_count > 0:
                 f.write(str(alignment) + '\n')
             fp2.close()
