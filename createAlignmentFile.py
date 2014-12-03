ed1c8b2bd3240f293f1fa1e687e2c8963cf97171
Diana Wan <jdwan@corn29.stanford.edu>
2014-12-02 19:39:00 -0800

    template3

--- createAlignmentFile.py
+++ createAlignmentFile.py
@@ -3,6 +3,10 @@ import os, time
 KB_STORAGE_DIR = 'kbTriples'
 TEXT_STORAGE_DIR = 'textTriples'
 XYZ_STORAGE_DIR = 'xyzTriples'
+KB_TRIPLES_DIR = 'kbTriples/'
+TEXT_TRIPLES_DIR = 'textTriples/'
+KB_CONSTANT_TRIPLES_DIR = 'kbConstantTriples/'
+CONST_THRESHOLD = 3
 
 def exactMatch(kb_directory, text_directory):
 
@@ -68,6 +72,80 @@ def alignment(kb_directory, text_directory, source):
             if intersect_count > 0:
                 f.write(str(alignment) + '\n')
             fp2.close()
+        fp1.close()
+
+# @params: kb_files
+# @return: a set of constant table file names to be used later in template3()
+def findConstantTables(kb_files, threshold):
+    const_files = set()
+
+    for kb_file in kb_files:
+        fp = open(KB_TRIPLES_DIR + kb_file, 'r')
+        argSet = set()
+
+        for line in fp:
+            # two arguments each line are separated by a tab
+            args = line.split('\t')
+            const = args[len(args) - 1].split('\n')[0]
+            argSet.add(const)
+        
+	# if the number of properties is not larger than a threshold, mark this as a const table
+        if(len(argSet) <= threshold):
+            const_files.add(kb_file)
+
+        fp.close()
+    
+    return const_files
+
+
+def template3(kb_files, text_files):
+    # TODO
+    # Aligns patterns of the form
+    #   X -> Y
+    #   |
+    #   v
+    #   C
+
+    # find constant tables
+    const_files = findConstantTables(kb_files, CONST_THRESHOLD)
+    
+    # create a dir to put results
+    if not os.path.exists(KB_CONSTANT_TRIPLES_DIR):
+        os.makedirs(KB_CONSTANT_TRIPLES_DIR)
+
+    # intersect all kbtables with const tables and create new tables
+    for kb_file in kb_files:
+        fp1 = open(KB_TRIPLES_DIR + kb_file, 'r')
+
+        for const_file in const_files:
+            if(const_file == kb_file):  # don't intersect a table with itself
+                continue
+
+            fp2 = open(KB_TRIPLES_DIR + const_file)
+            # create a map of the current const table
+            const_map = dict()
+
+            for const_line in fp2:
+                const_line_args = const_line.split('\t')
+                const_map[const_line_args[0]] = const_line_args[len(const_line_args) - 1].split('\n')[0]
+
+            # now check if the 2nd arg of kb_line exists in const_map
+            for kb_line in fp1:
+                kb_line_args = kb_line.split('\t')
+
+                if kb_line_args[1].split('\n')[0] in const_map:
+                    const_property = const_map[kb_line_args[1].split('\n')[0]]
+                    newTableName = kb_file + 'AND' + const_file + 'IS' + const_property
+                    f = open(KB_CONSTANT_TRIPLES_DIR + '/' + newTableName, 'a')
+                    alignment = dict()
+                    alignment['kb_arg0'] = kb_line_args[0]
+                    alignment['kb_arg1'] = kb_line_args[1]
+                    alignment['const_property'] = const_property
+                    f.write(str(alignment) + '\n')
+                    f.close()
+
+            fp2.close()
+
         fp1.close()
 
 
